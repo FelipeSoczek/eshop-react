e18700af5aaad1292cc67152e3e86d6b36989530
Felipe Soczek <felipecebulskisoczek@gmail.com>
2022-01-28 14:19:58 -0300

    rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <main>
-        <h1>Welcome to proshop</h1>
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' component={HomeScreen} exact />
+          </Routes>
+        </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
